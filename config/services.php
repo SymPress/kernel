dd0238b9b6fe74e2f71bfc5785fc3495f5f9243e
brianvarskonst <burnify.design@gmail.com>
2026-06-12 21:48:06 +0200

    chore: update package tooling

--- config/services.php
+++ config/services.php
@@ -8,6 +8,9 @@ use SymPress\Kernel\Resolver\ActivePackageResolver;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $container): void {
+    $parameters = $container->parameters();
+    $parameters->set('kernel.package_prefixes', []);
+
     $services = $container->services();
     $services->defaults()
         ->private()
@@ -16,6 +19,7 @@ return static function (ContainerConfigurator $container): void {
 
     $services->set(ActivePackageResolver::class);
     $services->set(PackageDiscovery::class)
+        ->arg('$packagePrefixes', '%kernel.package_prefixes%')
         ->public();
 
     $services->set(PackageManagerPage::class)
